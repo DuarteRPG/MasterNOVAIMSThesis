fe566a6e91d55112ff15774a390038a302f571a8
DuarteRPG <113636346+DuarteRPG@users.noreply.github.com>
2024-06-19 23:30:13 +0100

    Add files via upload

--- TesteDuarte_V2.py
+++ TesteDuarte_V2.py
@@ -2781,11 +2781,11 @@ elif Sport == 'Tennis':
             st.markdown("**Figure 20**: Relationship between Score and Rank. Score function, which provides a certain weight to each variable in the dataset, was defined by us, acccording to our knowledge of the sport. We tend to see an **inverse relationship between Score and Rank**, meaning that: \n - **the higher the Player's Score, the better tends to be the its rank**.")
             st.write("<div style='height: 150px;'></div>", unsafe_allow_html=True)
 
-            # # 21. SHARP: Top and Bottom 3 Individuals
-            # st.markdown(f"<h4 style='text-align: center;'>SHARP: Top and Bottom 3 Individuals</h1>", unsafe_allow_html=True)
-            # st.image("Top_bottom_feature_importance_5.png")
-            # st.markdown("**Figure 21**: Top 3 and Bottom 3 instances with their respective aggregate feature importance, providing insights on which are the most and the **least relevant features for their ranking**. For example:  \n - Features with a **high positive values among the top 3**, means that it was a **key feature** for these instances to achieve this **high/good ranking**; \n - Features with a **considerable negative values among the bottom 3**, means that it was a **key feature** for these instances to achieve this **low/bad ranking;** ")
-            # st.write("<div style='height: 150px;'></div>", unsafe_allow_html=True)
+            # 21. SHARP: Top and Bottom 3 Individuals
+            st.markdown(f"<h4 style='text-align: center;'>SHARP: Top and Bottom 3 Individuals</h1>", unsafe_allow_html=True)
+            st.image("Top_bottom_feature_importance_5.png")
+            st.markdown("**Figure 21**: Top 3 and Bottom 3 instances with their respective aggregate feature importance, providing insights on which are the most and the **least relevant features for their ranking**. For example:  \n - Features with a **high positive values among the top 3**, means that it was a **key feature** for these instances to achieve this **high/good ranking**; \n - Features with a **considerable negative values among the bottom 3**, means that it was a **key feature** for these instances to achieve this **low/bad ranking;** ")
+            st.write("<div style='height: 150px;'></div>", unsafe_allow_html=True)
 
             # # 22. SHARP: Feature Importance
             # st.markdown(f"<h4 style='text-align: center;'>SHARP: Feature Importance</h1>", unsafe_allow_html=True)
